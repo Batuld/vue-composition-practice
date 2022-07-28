9dbd7b1fb415d91c98727cae40f71953f2dd8c03
webelight-062 <you@example.com>
2022-07-28 19:00:04 +0530

    commposition api prac

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Timeline from "./components/Timeline.vue";
 // import { defineComponent } from "vue";
 
 // export default defineComponent({
@@ -18,23 +19,28 @@ import HelloWorld from "./components/HelloWorld.vue";
 //     return { name };
 //   },
 // });
-let name = "Batul";
+// let name = "Batul";
 </script>
 
 <template>
-  <div>
-    <h5>{{ name }}</h5>
-    <a href="https://vitejs.dev" target="_blank">
+  <header class="layout-header h-10">
+    <Timeline></Timeline>
+  </header>
+
+  <!-- <div> -->
+  <!-- <h5>{{ name }}</h5> -->
+
+  <!-- <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    </a>-->
+  <!-- </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
-<style scoped>
+<!-- <style scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -45,5 +51,5 @@ let name = "Batul";
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+} 
+</style>-->
---
+++ src/components/Timeline.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+// import { defineComponent } from "@vue/composition-api";
+import { ref, onMounted } from "vue";
+
+// export default defineComponent({
+//   setup() {},
+// });
+const periods = ["Today", "This Week", "This Month"];
+
+let selectedPeriod = ref("today");
+
+function selectePeriod(period: string) {
+  selectedPeriod.value = period;
+}
+
+//username code
+
+const username = ref("Denny");
+function updateUsername(newUsername: any) {
+  username.value = newUsername;
+}
+onMounted(() => {
+  updateUsername("Steven");
+});
+
+//modal code
+
+const isModalShown = ref(false);
+function showModal() {
+  isModalShown.value = true;
+}
+onMounted(() => {
+  showModal();
+});
+</script>
+
+<template>
+  <div class="text-3xl font-bold rounded shadow w-screen h-11 bg-blue-400">
+    {{ selectedPeriod }}
+    <a
+      v-for="period of periods"
+      :key="period"
+      class="
+        items-center
+        justify-center
+        p-2
+        text-base
+        font-normal
+        hover:text-slate-50
+      "
+      @click="selectePeriod(period)"
+      :class="{ 'is-active': period === selectedPeriod }"
+      >{{ period }}
+    </a>
+  </div>
+</template>
+<style scoped>
+a.is-active {
+  background: #fecdd3;
+}
+</style>
+
+<!-- options API -->
+
+<!-- <script>
+export default {
+    data() {
+        return {
+            username: 'Batul',
+            isModalShown: false
+        }
+    }
+},
+methods: {
+    updateUsername(newUsername){
+        this.username =newUsername
+    },
+    showModal(){
+        this.isModalShown = true
+    }
+},
+mounted(){
+    this.updateUsername('Steven')
+    this.showModal()
+}
+</script> -->
